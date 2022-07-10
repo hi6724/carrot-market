89e1e79efe296ff53c45bc1a1ef5e9fb6340aa4f
hi6724 <hi6724@gmail.com>
2022-07-10 15:48:08 +0900

    #3.1 타일윈드 셋업

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,tsx,ts,tsx}",
+    "./components/**/*.{js,tsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
