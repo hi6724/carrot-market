4fd2de6ff0d28d161d3410f96793c655d1be1437
hi6724 <hi6724@gmail.com>
2024-03-31 17:50:49 +0900

    ✨ 3.8 Lists and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,21 @@ import React from 'react';
 
 function Home() {
   return (
-    <main className='h-screen flex items-center justify-center p-5 bg-gray-100 sm:bg-cyan-100 md:bg-blue-100 lg:bg-green-100'>
-      <div className='bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row *:outline-none has-[:invalid]:bg-red-200'>
-        <input
-          className='w-full rounded-full py-3 bg-gray-200 pl-5 ring ring-transparent ring-offset-2 transition-shadow focus:ring-green-500 placeholder:font-bold
-          invalid:focus:ring-red-700 peer'
-          type='text'
-          required
-          placeholder='Email address'
-        />
-        <span className='text-red-500 font-medium hidden peer-invalid:block'>
-          Email is required.
-        </span>
-        <button
-          className='text-white py-2 rounded-full  transition-all font-medium focus:scale-90 bg-black md:px-10
-        peer-invalid:bg-red-900'
-        >
-          Login
-        </button>
+    <main className='h-screen flex items-center justify-center p-5 bg-gray-100'>
+      <div className='bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3 '>
+        {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => (
+          <div
+            key={index}
+            className='flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 rounded-xl border-b-2 p-2.5 pb-5 last:border-0 last:pb-2.5'
+          >
+            <div className='size-10 bg-blue-400 rounded-full' />
+            <span className='text-lg font-medium'>{person}</span>
+            <div className='size-6  bg-red-500 text-white flex items-center justify-center rounded-full relative'>
+              <span className='z-10'>{index}</span>
+              <div className='size-6 animate-ping rounded-full bg-red-500 absolute' />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
