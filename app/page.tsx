8e9954f799b11f405ed61c01e243faf8f046fc06
hi6724 <hi6724@gmail.com>
2024-03-31 16:22:38 +0900

    ✨ 3.6Form Modifiers (peer)

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,20 @@ function Home() {
     <main className='h-screen flex items-center justify-center p-5 bg-gray-100 sm:bg-red-100 md:bg-blue-100 lg:bg-green-100'>
       <div className='bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row'>
         <input
-          className='w-full rounded-full py-3 bg-gray-200 pl-5 outline-none ring ring-transparent ring-offset-2 transition-shadow focus:ring-orange-500 placeholder:font-bold'
-          type='text'
-          placeholder='Search here...'
+          className='w-full rounded-full py-3 bg-gray-200 pl-5 outline-none ring ring-transparent ring-offset-2 transition-shadow focus:ring-green-500 placeholder:font-bold
+          invalid:focus:ring-red-700 peer'
+          type='email'
+          required
+          placeholder='Email address'
         />
-        <button className=' bg-black text-white py-2 rounded-full  transition-transform font-medium focus:scale-90 outline-none md:px-10'>
-          Search
+        <span className='text-red-500 font-medium hidden peer-invalid:block'>
+          Email is required.
+        </span>
+        <button
+          className='text-white py-2 rounded-full  transition-all font-medium focus:scale-90 outline-none bg-black md:px-10
+        peer-invalid:bg-red-900'
+        >
+          Login
         </button>
       </div>
     </main>
