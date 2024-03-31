ab35460bf4c00054afe7494c188990bf2a11c35f
hi6724 <hi6724@gmail.com>
2024-03-31 14:41:23 +0900

    ✨ 3.2 card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main className='bg-gray-300 h-screen flex items-center justify-center p-5'>
+      <div className='bg-white w-full shadow-lg p-5 rounded-2xl'>
+        {/* header */}
+        <div className='flex justify-between items-center'>
+          <div className='flex flex-col'>
+            <span className='text-gray-600 font-semibold -mb-1'>
+              In transit
+            </span>
+            <span className='text-4xl font-semibold'>Coolblue</span>
+          </div>
+          <div className='rounded-full size-12 bg-orange-400' />
+        </div>
+        {/* 시간 */}
+        <div className='my-2 flex items-center gap-2'>
+          <span className='bg-green-400 text-white uppercase px-2 py-1.5 text-sm font-medium rounded-full'>
+            Today
+          </span>
+          <span>9:30-10:30</span>
+        </div>
+        {/* 로딩바 */}
+        <div className='relative'>
+          <div className='bg-gray-200 rounded-full w-full h-2 absolute' />
+          <div className='bg-green-400 rounded-full w-2/3 h-2 absolute' />
+        </div>
+        {/* hashTags */}
+        <div className='flex justify-between items-center mt-5 text-gray-600 text-sm'>
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span>Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
