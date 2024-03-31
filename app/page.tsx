af51a45510d64f6d3b764de5db291e577b535423
hi6724 <hi6724@gmail.com>
2024-03-31 18:02:34 +0900

    ✨ 3.9 Group Modifiers (group-focus-within)

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ function Home() {
         {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => (
           <div
             key={index}
-            className='flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 rounded-xl border-b-2 p-2.5 pb-5 last:border-0 last:pb-2.5'
+            className='flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 rounded-xl border-b-2 p-2.5 pb-5 last:border-0 last:pb-2.5 group'
           >
+            <input type='text' />
             <div className='size-10 bg-blue-400 rounded-full' />
-            <span className='text-lg font-medium'>{person}</span>
+            <span className='text-lg font-medium group-hover:text-red-500  group-focus-within:bg-cyan-300'>
+              {person}
+            </span>
             <div className='size-6  bg-red-500 text-white flex items-center justify-center rounded-full relative'>
               <span className='z-10'>{index}</span>
               <div className='size-6 animate-ping rounded-full bg-red-500 absolute' />
