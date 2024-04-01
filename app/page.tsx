4cf837ac51e0a910611f0c5c3d54dd7396803a18
hi6724 <hi6724@gmail.com>
2024-04-01 13:33:52 +0900

    ✨3.11 global.css 사용법

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,7 @@ function Home() {
     <main className='h-screen flex justify-center bg-hunmok pt-[60.5px] pb-hunmok '>
       <div className='bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3 '>
         {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => (
-          <div
-            key={index}
-            className='flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 rounded-xl border-b-2 p-2.5 pb-5 last:border-0 last:pb-2.5 group'
-          >
-            <input type='text' />
+          <div key={index} className='my-container group'>
             <div className='size-10 bg-blue-400 rounded-full' />
             <span className='text-lg font-medium group-hover:text-red-500  group-focus-within:bg-cyan-300'>
               {person}
@@ -18,8 +14,10 @@ function Home() {
               <span className='z-10'>{index}</span>
               <div className='size-6 animate-ping rounded-full bg-red-500 absolute' />
             </div>
+            <a href='#'>hello</a>
           </div>
         ))}
+        <button className='btn'>HELLO BUTTON</button>
       </div>
     </main>
   );
