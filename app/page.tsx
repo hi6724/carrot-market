7a54c4ac49b4ce175c0972dcfee81d418f59db28
hi6724 <hi6724@gmail.com>
2024-04-02 15:46:45 +0900

    4.0 HomeScreen

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,24 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <div>
-        <span>🥕</span>
-        <h1>당근</h1>
+    <div className='flex flex-col items-center justify-between min-h-screen p-5'>
+      <div className='my-auto *:font-medium flex flex-col items-center gap-2'>
+        <span className='text-6xl'>🥕</span>
+        <h1 className='text-4xl'>당근</h1>
         <h2>당근 마켓에 어서오세요!</h2>
       </div>
-      <div>
-        <Link href={'/create-account'}>시작하기</Link>
-        <div>
+      <div className='flex flex-col items-center gap-3 w-full'>
+        <Link
+          href={'/create-account'}
+          className='w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors'
+        >
+          시작하기
+        </Link>
+        <div className='flex gap-2'>
           <span>이미 계정이 있나요?</span>
-          <Link href={'/login'}>로그인</Link>
+          <Link href={'/login'} className='hover:underline '>
+            로그인
+          </Link>
         </div>
       </div>
     </div>
