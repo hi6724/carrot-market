9f01c52e7153e053584f2993350abdcd182215e0
hi6724 <hi6724@gmail.com>
2024-04-09 21:38:15 +0900

    ✨ Server Action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,36 +1,39 @@
-'use client';
 import FormButton from '@/components/form-btn';
 import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
 
 export default function LogIn() {
-  const onClick = async () => {
-    fetch('/www/users', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: 'NICO',
-        password: '123456',
-      }),
-    });
-  };
+  async function handleForm(formData: FormData) {
+    'use server';
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log('I RUN IN THE SERVER', formData);
+    console.log(formData.get('email'));
+    console.log(formData.get('password'));
+  }
+
   return (
     <div className='flex flex-col gap-10 py-8 px-6'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>안녕하세요!</h1>
         <h2 className='text-xl'>Log in with email and password.</h2>
       </div>
-      <form className='flex flex-col gap-3'>
-        <FormInput type='email' placeholder='Email' required errors={[]} />
+      <form action={handleForm} className='flex flex-col gap-3'>
         <FormInput
+          name='email'
+          type='email'
+          placeholder='Email'
+          required
+          errors={[]}
+        />
+        <FormInput
+          name='password'
           type='password'
           placeholder='Password'
           required
           errors={[]}
         />
+        <FormButton text='Log in' />
       </form>
-      <div onClick={onClick}>
-        <FormButton loading={false} text='Log in' />
-      </div>
       <SocialLogin />
     </div>
   );
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -5,13 +5,21 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
-function FormInput({ type, placeholder, required, errors }: FormInputProps) {
+function FormInput({
+  name,
+  type,
+  placeholder,
+  required,
+  errors,
+}: FormInputProps) {
   return (
     <div className='flex flex-col gap-2'>
       <input
         type={type}
+        name={name}
         placeholder={placeholder}
         required={required}
         className='bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 transition'
