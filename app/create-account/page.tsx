ba9d82c079a8c442469989f7f61e1c2d44d86cb1
hi6724 <hi6724@gmail.com>
2024-04-14 14:23:19 +0900

    ✨ zod 맛보기

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,18 +1,21 @@
+'use client';
 import FormButton from '@/components/form-btn';
 import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
-import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/24/solid';
-import Link from 'next/link';
 import React from 'react';
+import { useFormState } from 'react-dom';
+import { createAccount } from './action';
 
 function CreateAccountPage() {
+  const [state, dispatch] = useFormState(createAccount, null);
+
   return (
     <div className='flex flex-col gap-10 py-8 px-6'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>안녕하세요</h1>
         <h2 className='text-xl'>폼을 입력해주세요!</h2>
       </div>
-      <form className='flex flex-col gap-3'>
+      <form action={dispatch} className='flex flex-col gap-3'>
         <FormInput
           name='username'
           placeholder='Username'
@@ -28,7 +31,7 @@ function CreateAccountPage() {
         />
         <FormInput
           type='password'
-          name='Confirm'
+          name='confirm_password'
           placeholder='confirmPassword'
           required
         />
