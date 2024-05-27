3dca953de4d0b7ab12c648c280b456a5ed21c027
hi6724 <hi6724@gmail.com>
2024-05-27 17:42:31 +0900

    로그인action 리팩토링

--- app/login/action.ts
+++ app/login/action.ts
@@ -11,7 +11,34 @@ import bcrypt from 'bcrypt';
 import getSession from '@/lib/session';
 import { redirect } from 'next/navigation';
 
-const checkEmailExists = async (email: string) => {
+const checkPassword = async (
+  { email, password }: { email: string; password: string },
+  ctx: z.RefinementCtx
+) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      password: true,
+    },
+  });
+  const ok = await bcrypt.compare(password, user!.password ?? 'xxxx');
+  if (!ok) {
+    ctx.addIssue({
+      code: 'custom',
+      message: 'Password not correct',
+      path: ['password'],
+      fatal: true,
+    });
+  }
+};
+
+const checkEmailExists = async (
+  { email }: { email: string },
+  ctx: z.RefinementCtx
+) => {
   const user = await db.user.findUnique({
     where: {
       email,
@@ -20,22 +47,29 @@ const checkEmailExists = async (email: string) => {
       id: true,
     },
   });
-  return !!user;
+  if (!user) {
+    ctx.addIssue({
+      code: 'custom',
+      message: 'user Not Found',
+      path: ['email'],
+      fatal: true,
+    });
+    return z.NEVER;
+  }
 };
 
-const formSchema = z.object({
-  email: z
-    .string()
-    .email()
-    .toLowerCase()
-    .refine(checkEmailExists, 'An account with this email does not exist'),
-  password: z
-    .string({
-      required_error: '비밀번호를 입력해주세요',
-    })
-    .min(PASSWORD_MIN_LENGTH),
-  // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-});
+const formSchema = z
+  .object({
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string({
+        required_error: '비밀번호를 입력해주세요',
+      })
+      .min(PASSWORD_MIN_LENGTH),
+    // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+  })
+  .superRefine(checkEmailExists)
+  .superRefine(checkPassword);
 
 export async function login(prevState: any, formData: FormData) {
   const data = {
@@ -55,21 +89,9 @@ export async function login(prevState: any, formData: FormData) {
         password: true,
       },
     });
-    const ok = await bcrypt.compare(
-      result.data.password,
-      user!.password ?? 'xxxx'
-    );
-
-    if (ok) {
-      const session = await getSession();
-      session.id = user!.id;
-      await session.save();
-      redirect('/profile');
-    }
-    return {
-      fieldErrors: {
-        password: ['Wrong Password'],
-      },
-    };
+    const session = await getSession();
+    session.id = user!.id;
+    await session.save();
+    redirect('/profile');
   }
 }
