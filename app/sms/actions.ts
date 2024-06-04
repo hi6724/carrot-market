48f60a7393cd3cfeb5837835e877757cc8dc6707
hi6724 <hi6724@gmail.com>
2024-06-04 17:07:14 +0900

    ✨ 9.5 SMS Token

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import crypto from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
+import db from '@/lib/db';
 import { redirect } from 'next/navigation';
 import validator from 'validator';
 import { z } from 'zod';
@@ -16,6 +19,18 @@ const tokenSchema = z.coerce.number().min(100000).max(999999);
 interface ActionState {
   token: boolean;
 }
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: { token },
+    select: { id: true },
+  });
+  if (exists) {
+    return getToken();
+  } else {
+    return token;
+  }
+}
 
 export async function smsLogin(prevState: ActionState, formData: FormData) {
   const phone = formData.get('phone');
@@ -23,7 +38,31 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
   if (!prevState.token) {
     const result = phoneSchema.safeParse(phone);
     if (!result.success) return { token: false, error: result.error.flatten() };
-    else return { token: true };
+    else {
+      // delete 이전토큰
+      await db.sMSToken.deleteMany({
+        where: { user: { phone: result.data } },
+      });
+      // create token
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: { phone: result.data },
+              create: {
+                username: uuidv4(),
+                nickname: 'Incognito',
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+      // send token
+      return { token: true };
+    }
   } else {
     const result = tokenSchema.safeParse(token);
     if (!result.success) return { token: true, error: result.error.flatten() };
