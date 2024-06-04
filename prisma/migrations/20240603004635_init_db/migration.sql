351181e0a1f1c67211658d4e624046db9f1171d3
hi6724 <hi6724@gmail.com>
2024-06-04 15:07:09 +0900

    🔧 db변경, uuid설치

--- prisma/migrations/20240603004635_init_db/migration.sql
+++ prisma/migrations/20240603004635_init_db/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "username" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
     "email" TEXT,
     "password" TEXT,
     "phone" TEXT,
@@ -9,6 +10,16 @@ CREATE TABLE "User" (
     "avatar" TEXT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "SMSToken" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "token" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "SMSToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
@@ -22,3 +33,6 @@ CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_github_id_key" ON "User"("github_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SMSToken_token_key" ON "SMSToken"("token");
